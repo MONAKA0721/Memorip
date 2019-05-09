ee4bbf9bb36d07376f6fdb62d57734f5edcaa528
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-05-09 15:47:00 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
