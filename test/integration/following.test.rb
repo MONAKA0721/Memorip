b3aaa6638d7336cd7f52e6df4fd6a8938cb6cab9
住江 祐哉 <yuya@sumienoMacBook-puro.local>
2019-05-25 14:08:29 +0900

    add test

---
+++ test/integration/following.test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class FollowingTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    log_in_as(@user)
+  end
+  .
+  .
+  .
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), respons.body
+    end
+  end
+end
