a6a94c2ad1b5be444118ac9a421e35ff94bdbad2
Yuya Sumie <monamonacan0926@gmail.com>
2019-07-03 11:36:53 +0900

    マーカーとウィンドウの表示

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -5,14 +5,63 @@ geocoderを用意
 
 let map
 let geocoder
+var marker = [];
+var infoWindow = [];
+var markerData = [ // マーカーを立てる場所名・緯度・経度
+  {
+       name: 'TAM 東京',
+       lat: 35.6954806,
+        lng: 139.76325010000005,
+ }, {
+        name: '小川町駅',
+     lat: 35.6951212,
+        lng: 139.76610649999998
+ }, {
+        name: '淡路町駅',
+     lat: 35.69496,
+      lng: 139.76746000000003
+ }, {
+        name: '御茶ノ水駅',
+        lat: 35.6993529,
+        lng: 139.76526949999993
+ }, {
+        name: '神保町駅',
+     lat: 35.695932,
+     lng: 139.75762699999996
+ }, {
+        name: '新御茶ノ水駅',
+       lat: 35.696932,
+     lng: 139.76543200000003
+ }
+];
 
 function initMap(){
   // geocoderを初期化
   geocoder = new google.maps.Geocoder()
 
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -34.397, lng: 150.644},
-  zoom: 8
+  center: {lat: 35.6954806, lng: 139.76325010000005},
+  zoom: 15
+  });
+
+  for (var i = 0; i < markerData.length; i++) {
+        markerLatLng = new google.maps.LatLng({lat: markerData[i]['lat'], lng: markerData[i]['lng']}); // 緯度経度のデータ作成
+        marker[i] = new google.maps.Marker({ // マーカーの追加
+         position: markerLatLng, // マーカーを立てる位置を指定
+            map: map // マーカーを立てる地図を指定
+       });
+
+     infoWindow[i] = new google.maps.InfoWindow({ // 吹き出しの追加
+         content: '<div class="sample">' + markerData[i]['name'] + '</div>' // 吹き出しに表示する内容
+       });
+
+     markerEvent(i); // マーカーにクリックイベントを追加
+ }
+}
+
+function markerEvent(i) {
+    marker[i].addListener('click', function() { // マーカーをクリックしたとき
+      infoWindow[i].open(map, marker[i]); // 吹き出しの表示
   });
 }
 
