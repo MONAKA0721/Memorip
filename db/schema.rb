66d38502dcfa87e8d042fb765aa83809f6e2941a
Yuya Sumie <monamonacan0926@gmail.com>
2019-08-06 23:18:38 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190618130421) do
+ActiveRecord::Schema.define(version: 20190721092539) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20190618130421) do
     t.integer "prefecture_code"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "plans", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "relationships", force: :cascade do |t|
