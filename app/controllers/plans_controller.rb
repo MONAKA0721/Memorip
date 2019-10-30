983913b12155e44351782e4f6360cdefe329a1f2
MONAKA0721 <monamonacan0926@gmail.com>
2019-10-30 15:26:28 +0900

    maps_controller, planning_controller を plans_controller に併合

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,7 +2,37 @@ class PlansController < ApplicationController
   def update
     @plan = Plan.first
     @plan.update_attributes(plan_params)
-    redirect_to controller: 'maps', action: 'index'
+    redirect_to controller: 'plans', action: 'index'
+  end
+
+  def index
+    if params[:markerName]
+      gon.markerName = params[:markerName]
+    end
+    if params[:markerData]
+      gon.Data = params[:markerData]
+    else
+      gon.Data =[
+           {name: 'TAM 東京',
+           lat: 35.6954806,
+            lng: 139.76325010000005
+          }
+        ]
+    end
+
+    @plan = Plan.first
+    gon.planData = [
+      @plan.destination1,
+      @plan.destination2,
+      @plan.destination3,
+      @plan.destination4,
+      @plan.destination5,
+      @plan.destination6,
+      @plan.destination7,
+      @plan.destination8,
+      @plan.destination9,
+      @plan.destination10
+      ]
   end
 
   private
