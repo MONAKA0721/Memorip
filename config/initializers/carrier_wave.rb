1bc10295dc27c35977230a2fdf93892e10ed31fd
MONAKA0721 <monamonacan0926@gmail.com>
2019-11-08 12:30:20 +0900

    carrier_wave のバグ修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,8 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    
-    # This is necessary for sitemap_generator
-    config.storage = :fog
 
     config.fog_credentials = {
       # Amazon S3用の設定
@@ -12,5 +9,7 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    # This is necessary for sitemap_generator
+    config.storage = :fog
   end
 end
