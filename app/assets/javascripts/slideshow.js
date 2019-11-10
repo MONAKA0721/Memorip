274dd0bb62ff6a192944e43503ff4bfb8cfb82f6
山本浩平 <>
2019-11-10 11:41:48 +0900

    backgroundスライドショーの設定

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,11 +1,13 @@
 $(function(){
-  var imgs = $("#slideshow > li");
-  var imgLen = imgs.length;
-  var count = 0;
-  function changeImg(){
-    //スライド切替のポイント（配列を順番に処理し終わったら配列の先頭に戻る計算式）
-    count = (count + 1) % imgLen;
-    imgs.removeClass("showSlide").eq(count).addClass("showSlide");
-  }
-  setInterval(changeImg, 5000); //切替の間隔（ミリ秒）
-})
+  $('body').vegas({ //背景画像でスライドショーしたい場所の設定
+    slides: [
+     { src: '/Hokkaido.jpg' }, //スライドする画像を配列で設定
+     { src: '/Kyoto.jpg' },
+     { src: '/Okinawa.jpg' }
+   ],
+     delay: 6000, //スライドまでの時間ををミリ秒単位で設定
+     timer: true,
+     transition: 'blur', //スライド間のエフェクトを設定
+     transitionDuration: 3000 //エフェクト時間をミリ秒単位で設定
+  });
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( gmap.js )
+Rails.application.config.assets.precompile += %w( slideshow.js )
 # Rails.application.config.assets.precompile += %w( microposts.scss )
