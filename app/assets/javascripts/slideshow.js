78bc99f1c15f494394b2ab2ab807130872c4f520
山本浩平 <>
2019-11-13 00:17:07 +0900

    レイアウト修正

--- app/assets/javascripts/slideshow.js
+++ app/assets/javascripts/slideshow.js
@@ -1,13 +1,15 @@
 $(function(){
-  $('body').vegas({ //背景画像でスライドショーしたい場所の設定
+  $('#home_body').vegas({ //背景画像でスライドショーしたい場所の設定
     slides: [
      { src: '/Hokkaido.jpg' }, //スライドする画像を配列で設定
-     { src: '/Kyoto.jpg' },
+     { src: '/Fuji.jpg' },
      { src: '/Okinawa.jpg' }
    ],
      delay: 6000, //スライドまでの時間ををミリ秒単位で設定
-     timer: true,
      transition: 'blur', //スライド間のエフェクトを設定
-     transitionDuration: 3000 //エフェクト時間をミリ秒単位で設定
+     transitionDuration: 3000, //エフェクト時間をミリ秒単位で設定
+     cover: true,
+     timer: false,
+     overlay: 'http://jaysalvat.github.io/vegas/releases/latest/overlays/06.png'
   });
 });
