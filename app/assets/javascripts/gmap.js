61effe64b4e239be4121d5dcd433765e25260298
Yuya Sumie <monamonacan0926@gmail.com>
2019-11-12 17:18:41 +0900

    プラン作成画面でも Google Map を使用できるようになった

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -2,7 +2,6 @@ var map
 var geocoder
 var bounds
 var marker = [];
-var planData = gon.planData;
 var planDestinationPosition = [];
 
 
@@ -14,64 +13,52 @@ function isPlanDataNull(array){
   if(count === array.length) return true;
 }
 
-function initMap(){
+function initMap(planData){
   geocoder = new google.maps.Geocoder();
-  //console.log(planDestinationPosition);
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 35.6954806, lng: 139.76325010000005},
-  zoom: 12
+    center: {lat: 35.6954806, lng: 139.76325010000005},
+    zoom: 12
   });
 
   if(!isPlanDataNull(planData)){
-      map = new google.maps.Map(document.getElementById("map"));
-      bounds = new google.maps.LatLngBounds();
+    map = new google.maps.Map(document.getElementById("map"));
+    bounds = new google.maps.LatLngBounds();
 
-      for(let i = 0 ; i < planData.length ; i++){
-        console.log(planData[i])
-        geocoder.geocode( { 'address': planData[i] }, function(results, status) {
-          if (status == 'OK'){
-            bounds.extend(results[0].geometry.location);
-            map.fitBounds (bounds);
+    for(let i = 0 ; i < planData.length ; i++){
+      console.log(planData[i])
+      geocoder.geocode( { 'address': planData[i] }, function(results, status) {
+        if (status == 'OK'){
+          bounds.extend(results[0].geometry.location);
+          map.fitBounds (bounds);
 
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                label: `${i + 1}`
-            });
-            //planDestinationPosition.push(results[0])
+          var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            label: `${i + 1}`
+          });
 
-            var infowindow = new google.maps.InfoWindow({
-              content: '<ul>'+ '<li>' + planData[i] + '</li>' + '<li>' + results[0].formatted_address + '</li>' + '</ul>'
-            });
+          var infowindow = new google.maps.InfoWindow({
+            content: '<ul>'+ '<li>' + planData[i] + '</li>' + '<li>' + results[0].formatted_address + '</li>' + '</ul>'
+          });
 
-            marker.addListener('click', function() {
-              infowindow.open(map, marker);
-            });
-          }
-        });
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
+        }
+      });
     }
   }
 }
 
 function codeAddress(){
-  // 入力を取得
   var inputAddress = document.getElementById('address').value;
 
-  // geocodingしたあとmapを移動
   geocoder.geocode( { 'address': inputAddress}, function(results, status) {
     if (status == 'OK') {
-　　　　　　　　　　　　// map.setCenterで地図が移動
       map.setCenter(results[0].geometry.location);
-
-　　　　　　　　　　　　// google.maps.MarkerでGoogleMap上の指定位置にマーカが立つ
-      // var marker = new google.maps.Marker({
-      //     map: map,
-      //     position: results[0].geometry.location
-      // });
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
-
     document.getElementById('address').value = '';
   });
 }
@@ -81,12 +68,10 @@ function addMarker(){
   var inputAddress = document.getElementById('markerAddress').value;
   geocoder.geocode( { 'address': inputAddress}, function(results, status) {
     if (status == 'OK'){
-　　　　　　　　　　　　// map.setCenterで地図が移動
       map.setCenter(results[0].geometry.location);
-　　　　　　　　　　　　// google.maps.MarkerでGoogleMap上の指定位置にマーカが立つ
       var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+        map: map,
+        position: results[0].geometry.location
       });
 
       var infowindow = new google.maps.InfoWindow({
@@ -111,6 +96,21 @@ function addMarker(){
   document.getElementById('markerAddress').value = '';
 }
 
+function getDestinations() {
+  let destinations = [];
+  for(let i = 0; i < 10; i++){
+    let id = 'plan_destinations_attributes_' + i + '_name';
+    if(document.getElementById(id).value){
+      destinations.push(document.getElementById(id).value)
+    }
+  }
+  return destinations;
+}
+
+function updateMap(){
+  const destinations = getDestinations();
+  initMap(destinations)
+}
 window.onload = function(){
-      initMap();
-  };
+  initMap(gon.planData);
+};
