8875a01f861d40ef38c26c253616d56f25b66bae
山本浩平 <>
2019-12-04 03:05:27 +0900

    Memoripについてページのファイルだけ作成,サムネイル選択部分レイアウト

--- app/assets/javascripts/image_preview.js
+++ app/assets/javascripts/image_preview.js
@@ -38,3 +38,28 @@ $(function(){
     }
   }
   });
+
+  $(function(){
+    $('#thumbnail').change(function(e){
+      //ファイルオブジェクトを取得する
+      var file = e.target.files[0];
+      var reader = new FileReader();
+
+      //画像でない場合は処理終了
+      if(file.type.indexOf("image") < 0){
+        alert("画像ファイルを指定してください。");
+        return false;
+      }
+
+      //アップロードした画像を設定する
+      reader.onload = (function(file){
+        return function(e){
+          $('#img1').removeAttr("src","thumbimg.png");
+          $("#img1").attr("src", e.target.result);
+          $("#img1").attr("title", file.name);
+        };
+      })(file);
+      reader.readAsDataURL(file);
+
+    });
+  });
