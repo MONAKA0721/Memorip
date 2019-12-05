a7f2c492c73622a3024da20162b465933bea0dae
山本浩平 <>
2019-12-05 16:29:06 +0900

    お問い合わせがメールで来るようにした2

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,12 +2,13 @@ class InquiryMailer < ApplicationMailer
   # class InquiryMailer < ActionMailer::Base
     # これ入れないと送れいない。
     default from: "info@heroku.com"
-    default to: "memorip1104@gmail.com"
+    # default to: "memorip1104@gmail.com"
     # mail(to: @inquiry.email, を入れたのでいらないかも。
 
     def received_email(inquiry)
       @inquiry = inquiry
       # mail(to: @inquiry.email, :subject => 'お問い合わせありがとうございます。')
       mail(to: @inquiry.email, subject: 'お問い合わせありがとうございます。')
+      mail(to: 'memorip1104@gmail.com', subject: 'お問い合わせメールが来ています。')
     end
   end
