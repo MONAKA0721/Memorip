790346dfad97562a6ebbda20af756e86ac36d470
山本浩平 <>
2019-12-05 16:20:13 +0900

    問い合わせがgmailに届くようにした

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
   # class InquiryMailer < ActionMailer::Base
     # これ入れないと送れいない。
     default from: "info@heroku.com"
-    # default to: "送信先"
+    default to: "memorip1104@gmail.com"
     # mail(to: @inquiry.email, を入れたのでいらないかも。
 
     def received_email(inquiry)
